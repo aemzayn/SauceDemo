c15fcb7c9047340c619c92d8092c7f96586c07b9
Muhammed Furkan Güler <79373693+thenqlv@users.noreply.github.com>
2022-11-25 21:24:39 +0300

    Bir cypress uygulamasıdır.
    Cypress öğrenmek amacıyla https://www.saucedemo.com/ sayfası için bazı testler yazılmıştır.

---
+++ SauceDemo/cypress/support/commands.js
@@ -0,0 +1,37 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import {Login} from "../pageObjects/login";
+
+const login = new Login();
+
+Cypress.Commands.add('login',(user) => {
+    login.typeUsernameTextBox(user.username)
+    login.typePasswordTextBox(user.password)
+    login.clickLoginButton()
+})
+
+
